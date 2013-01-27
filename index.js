9be147ea9eb635ae8f2a5693064fd4845c7a08eb
Sergey Stamat <sergey.stamat@gmail.com>
2013-01-27 02:59:28 +0200

    array support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var util = require('util');
 var toxml = function(obj) {
 	var xml = "";
-	if(typeof obj == 'object') {
+	if(typeof obj == 'object' && obj instanceof Array) {
+        
+    } else if(typeof obj == 'object') {
 		for(var key in obj) {
 			if(key != "_attr" && key != "_val") {
 				if(typeof obj[key]['_attr'] != 'undefined') {
@@ -13,8 +15,14 @@ var toxml = function(obj) {
 				} else {
 					var attrs = "";
 				}
-				xml+= util.format("<%s%s>%s</%s>", key, attrs,
-					toxml(typeof obj[key]["_val"] == 'undefined' ? obj[key] : obj[key]["_val"]), key);
+	            if(obj[key] instanceof Array) {
+                    for(var t = 0; t < obj[key].length; t++) {
+                        xml+= util.format("<%s>%s</%s>", key, toxml(obj[key][t]), key);
+                    }
+                } else {
+    				xml+= util.format("<%s%s>%s</%s>", key, attrs,
+	    				toxml(typeof obj[key]["_val"] == 'undefined' ? obj[key] : obj[key]["_val"]), key);
+                }
 			}
 		}
 	} else if(typeof obj=='number' || typeof obj=='string') {
